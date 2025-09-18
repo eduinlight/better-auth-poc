afede9e0d71f7f6a80a08cf61e2dcf0dc2e4886c
Eduin Garcia Cordero <eduinlight@gmail.com>
2025-09-18 19:20:56 +0200

    fixed auth provider

--- apps/client/src/providers/AuthProvider.tsx
+++ apps/client/src/providers/AuthProvider.tsx
@@ -1,12 +1,22 @@
 import { AuthUIProvider } from "@daveyplate/better-auth-ui";
 import { authClient } from "@package/auth-client";
 import type { FC, PropsWithChildren } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, NavLinkProps, useNavigate } from "react-router-dom";
+
+
+function RouterLink({ href, children, ...props }: { href: string; children: React.ReactNode }) {
+  return (
+    <NavLink to={href} {...props}>
+      {children}
+    </NavLink>
+  )
+}
 
 export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 	const navigate = useNavigate();
+  console.log({navigate})
 	return (
-		<AuthUIProvider authClient={authClient} navigate={navigate} Link={NavLink}>
+		<AuthUIProvider authClient={authClient} navigate={navigate} Link={RouterLink}>
 			{children}
 		</AuthUIProvider>
 	);
